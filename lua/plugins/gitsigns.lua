bf1f177f19530609d7060d80f9df7b28cfa43647
theandrunique <theandrunique@gmail.com>
2024-09-28 18:22:24 +0000

    ~

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,8 +4,8 @@ return {
     config = function()
       require("gitsigns").setup({
         signs = {
-          add          = { text = '+' },
-          change       = { text = '~' },
+          add          = { text = '┃' },
+          change       = { text = '┃' },
           delete       = { text = '_' },
           topdelete    = { text = '‾' },
           changedelete = { text = '~' },
@@ -15,7 +15,7 @@ return {
         current_line_blame_opts = {
           virt_text = true,
           virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-          delay = 1000,
+          delay = 100,
           ignore_whitespace = false,
           virt_text_priority = 100,
           use_focus = true,
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local themes = require("telescope.themes")
+
+    local builtin = require("telescope.builtin")
+    vim.g.telescope = builtin
+
+    vim.keymap.set("n", "<leader>p", function()
+      builtin.find_files(themes.get_dropdown())
+    end, {})
+
+    vim.keymap.set("n", "<leader>/", function()
+      builtin.live_grep(themes.get_dropdown())
+    end, {})
+
+    vim.keymap.set("n", "<C-f>", function()
+      builtin.resume()
+    end, {})
+
+    vim.keymap.set("n", "<leader>b", function()
+      builtin.buffers(themes.get_dropdown())
+    end, {})
+
+  end
+}
+
