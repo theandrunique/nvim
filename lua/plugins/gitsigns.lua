eda97bdb6d28d9d60ca18c7170abc70436f0bee1
theandrunique <theandrunique@gmail.com>
2024-09-28 18:25:55 +0000

    ~

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -20,7 +20,7 @@ return {
           virt_text_priority = 100,
           use_focus = true,
         },
-        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+        current_line_blame_formatter = '<author>, <author_time:%R> • <summary>',
       })
     end
   },
