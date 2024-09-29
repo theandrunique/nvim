74f4d957ee45125f409fa7fe48364a6b41cf15d9
theandrunique <theandrunique@gmail.com>
2024-09-29 17:53:37 +0000

    Add mini.nvim

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,11 @@
+return {
+    "echasnovski/mini.nvim",
+    config = function()
+        require("mini.ai").setup()
+        require("mini.surround").setup()
+        require("mini.statusline").setup({
+            use_icons = true,
+        })
+    end
+}
+
